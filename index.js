1c07fa75ea988db90f6a3b859a35e47453cfcd34
NikolaGjelov <niko_gelco@hotmail.com>
2023-02-24 22:07:01 +0100

    ah

---
+++ index.js
@@ -0,0 +1,12 @@
+let inNumber = document.querySelector(".input")
+let btn = document.querySelector(".btn")
+let result = ""
+btn.addEventListener("click", function () {
+if (inNumber.value % 2 == 0) {
+  result=("EVEN NUMBER!")}
+else
+  {
+  result=("ODD NUMBER!")
+  }
+document.querySelector(".par").innerHTML=result
+})
